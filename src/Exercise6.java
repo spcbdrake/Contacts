354f922e69693eac3b1339d3fd54459b31eaf169
benjamindrake <spcbdrake@yahoo.com>
2015-10-28 11:07:01 -0400

    ex6 diff methods

--- src/Exercise6.java
+++ src/Exercise6.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * Created by benjamindrake on 10/28/15.
@@ -10,6 +13,12 @@ public class Exercise6 {
         ArrayList<String> names = new ArrayList(Arrays.asList("alice", "bob", "charlie", "david", "allen", "barbie", "colin", "dan"));
         //Take the first 5(in one command)
         ArrayList<String> newNames = new ArrayList(names.subList(0, 5));
+
+        solveViaStream(newNames);
+        solveViaTempLists(newNames);
+        solveViaDirectModification(newNames);
+
+
         //Make every String inside uppercase
         ArrayList<String> tempNames = new ArrayList();
         for (String name : newNames) {
@@ -32,8 +41,55 @@ public class Exercise6 {
                 it.remove();
             }
         }
+    }
 
+    static void solveViaDirectModification(ArrayList<String> newNames) {
+        for (int i = 0; i < newNames.size(); i++) {
+            String s = newNames.get(i).toUpperCase();
+            newNames.set(i, s);
+        }
+
+        Iterator<String> it = newNames.iterator();
+        while (it.hasNext()) {
+            String name = it.next();
+            if (name.startsWith("A")) {
+                it.remove();
+            }
+        }
         System.out.println(newNames);
+    }
 
+
+    static void solveViaTempLists(ArrayList<String> newNames) {
+        //Make every String inside uppercase
+        ArrayList<String> tempNames = new ArrayList();
+        for (String name : newNames) {
+            tempNames.add(name.toUpperCase());
+        }
+        newNames = tempNames;
+        //Remove the ones starting with "A"
+        tempNames = new ArrayList();
+        for (String name : newNames) {
+            if (!name.startsWith("A")) {
+                tempNames.add(name);
+            }
+        }
+        newNames = tempNames;
+        System.out.println(newNames);
+    }
+
+    static void solveViaStream (ArrayList<String> newNames) {
+        List<String> newNewNames =
+                newNames
+                .stream()
+                .map((name) -> {
+                    return name.toUpperCase();
+                })
+                .filter((name) -> {
+                    return !name.startsWith("A");
+                })
+                .collect(Collectors.toList());
+
+        System.out.println(newNewNames);
     }
 }
